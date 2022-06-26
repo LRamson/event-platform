94d56ec9baf87c25db5d76995b342b434143d445
LRamson <lucasramson@gmail.com>
2022-06-26 15:23:10 -0300

    Integração do backend com o componente Video

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { ApolloProvider } from "@apollo/client"
+import { client } from "./lib/apollo"
+
+import { BrowserRouter } from "react-router-dom"
+
 import { Event } from "./pages/Event"
+
+import { Router } from "./Router"
 
 function App() {
   return (
-    <Event/>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
   )
 }
 
